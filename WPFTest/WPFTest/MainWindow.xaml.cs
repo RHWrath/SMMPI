203879a94f1d43adf2ca45802bd5627b2001b32c
Koen Sampers <neoks23@gmail.com>
2026-03-31 19:12:27 +0200

    WPFTest
    feat: WPFTest project added to the parttime repository

---
+++ WPFTest/WPFTest/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using SMM.Data;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        [DllImport("kernel32.dll")]
+        static extern bool AllocConsole();
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            AllocConsole();  // Call to open console
+            Console.WriteLine("Console opened!");
+
+            ComboBox1.ItemsSource = Enum.GetValues(typeof(LogCategory));
+            ComboBox1.SelectedIndex = 0;
+        }
+
+        private void InitApplication(object sender, RoutedEventArgs e)
+        {
+            
+            var target = new TargetData();
+
+            target.Feed(LogCategory.Chat, new ChatLog("Hi from Discord", "user123", DateTime.Now));
+            target.Feed(LogCategory.Chat, new ChatLog("Hi my name is Chuck", "user124", DateTime.Now));
+
+            target.Feed(LogCategory.User, new UserLog("user123", "NicolasCage", DateOnly.FromDateTime(DateTime.Now),
+                "discord,steam,etc...", DateTime.Now));
+            target.Feed(LogCategory.User, new UserLog("user124", "ChuckNorriss", DateOnly.FromDateTime(DateTime.Now),
+                "discord,steam,etc...", DateTime.Now));
+
+            target.Feed(LogCategory.Recording, new MediaLog(File.ReadAllBytes("C:\\Users\\neoks\\Videos\\test.mkv"), "video/mkv", DateTime.Now));
+            target.Feed(LogCategory.Recording, new MediaLog(File.ReadAllBytes("C:\\Users\\neoks\\Videos\\test.mkv"), "video/mp4", DateTime.Now));
+            target.Feed(LogCategory.Recording, new MediaLog(File.ReadAllBytes("C:\\Users\\neoks\\Videos\\test.mkv"), "video/wav", DateTime.Now));
+            
+            foreach (var log in target.ExtractLogs((LogCategory)ComboBox1.SelectedValue))
+                Console.WriteLine(log.ToString());
+        }
+    }
+}
---
+++ WPFTest/WPFTest/TargetData.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMM.Data  // Bundles like Kotlin package
+{
+    public enum LogCategory
+    {
+        Chat,     
+        User,     
+        Recording,
+        Error         
+    }
+
+    public abstract record Log(DateTime Timestamp);
+
+    public record LogEntry(LogCategory Category, DateTime Timestamp, Log LogRecord);
+
+    public record MediaLog(byte[] Data, string MimeType, DateTime Timestamp)
+        : Log(Timestamp)
+    {
+        public override string ToString() => $"MimeType: {MimeType}, Timestamp:[{Timestamp:HH:mm:ss}]";
+    }
+    public record ChatLog(string Content, string UserId, DateTime Timestamp)
+    : Log(Timestamp)
+    {
+        public override string ToString() => $"UID: {UserId}, Content:{Content}, Timestamp:[{Timestamp:HH:mm:ss}]";
+    }
+    public record UserLog(string UserId, string Username, DateOnly MemberSince, string connections, DateTime Timestamp)
+    : Log(Timestamp)
+    {
+        public override string ToString() => $"UID: {UserId},Username: {Username}, MemberSince: {MemberSince}, " +
+            $"Connections: {connections}, Timestamp:[{Timestamp:HH:mm:ss}]";
+    }
+
+    public class TargetData
+    {
+        internal List<LogEntry> Logs { get; } = new();
+
+        private void AddLog(LogCategory category, Log log)
+        {
+            Logs.Add(new LogEntry(category,DateTime.Now,log));
+        }
+
+        public void Feed(LogCategory category, Log? log)
+        {
+            switch ((category, log))
+            {
+                case (LogCategory.Chat, ChatLog cl):
+                    AddLog(LogCategory.Chat, cl);
+                    break;
+                case (LogCategory.User, UserLog ul):
+                    AddLog(LogCategory.User, ul);
+                    break;
+                case (LogCategory.Recording, MediaLog ml):
+                    AddLog(LogCategory.Recording, ml);
+                    break;
+            }
+        }
+
+        public IEnumerable<LogEntry> ExtractLogs(LogCategory category)
+        {
+            return Logs
+                .Where(entry => entry.Category == category)
+                .Select(entry => entry)
+                .Where(data => data != null)!;
+        }
+    }
+}
